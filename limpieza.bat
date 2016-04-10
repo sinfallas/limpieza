ff4f1de8b0ab237446093e0a4b89cecd43cd4ece
jose perez <sinfallas@gmail.com>
2016-04-10 19:26:12 -0430

    * se limpia internet explorer

--- limpieza.bat
+++ limpieza.bat
@@ -1,20 +1,27 @@
 @echo off
-net stop spooler
-del %systemroot%system32spoolPRINTERS*.* /Q
-net start spooler
-
 ipconfig /flushdns
+net stop dnscache
+dnscache net start
+
+net stop spooler
+del %SystemRoot%\system32\spool\PRINTERS\*.* /Q
+net start spooler
 
 cd c:\usuarios\%username%\AppData\Local
 rmdir /S /Q Temp
-
 cd ..
 
-cd windows
-del *.tmp /s /f /q
-del *.bak /s /f /q
-del *.old /s /f /q
+del %TEMP%\* /s /f /q
+del c:\windows\*.tmp /s /f /q
+del c:\windows\*.bak /s /f /q
+del c:\windows\*.old /s /f /q
 
-cd ..
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 2
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 1
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 16
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 32
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 255
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 4351
 
 exit
